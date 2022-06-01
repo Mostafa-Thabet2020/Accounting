700bf5819b2ff5369e638a69d17e111335fd8d55
Mostafa-Thabet2020 <62522809+Mostafa-Thabet2020@users.noreply.github.com>
2022-06-01 22:48:48 +0200

    Add project files.

---
+++ Accounting.Test/AccountingOperationTest.cs
@@ -0,0 +1,30 @@
+﻿using Accounting.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Test
+{
+    internal class AccountingOperationTest
+    {
+        [Test]
+        public void SumTwoValuesTest()
+        {
+            AccountingOperation accountingOperation = new AccountingOperation();
+
+            int result = accountingOperation.SumTwoValues(4, 5);
+            Assert.AreEqual(9, result);
+        }
+
+        [TestCase(5,5,ExpectedResult =10)]
+        [TestCase(5,20,ExpectedResult =25)]
+        public int SumTwoValuesTestCases(int x, int y)
+        {
+            AccountingOperation accountingOperation = new AccountingOperation();
+
+            return accountingOperation.SumTwoValues(x,y);
+        }
+    }
+}
